82eb26cec29682b4306ed51e3cb320b04fe8d21f
PhoenixMo <1002375424@qq.com>
2016-09-18 14:51:24 +0800

    Finish HomeWork 2

--- mian/main.ts
+++ mian/main.ts
@@ -1,2 +1,21 @@
-var width = 100;
-console.log("what");
+var S="*";
+var K=" ";
+
+ console.log("就先...强行只输5行吧");
+var n:number =5;
+
+for(var i=1;i<=n;i++)
+{
+    var C="";
+    for(var j=1;j<=(n+i-1);j++)
+    {
+        if((n-i+1)<=j&&j<=(n+i-1))
+        {
+            C=C+S;
+        }else
+        {
+            C=C+K;
+        }
+    }
+    console.log(C);
+}
